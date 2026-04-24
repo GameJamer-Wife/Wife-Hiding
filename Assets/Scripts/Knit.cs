09a5d0b01362badcc895f304c7a976ab9d8f2c71
CasperBohlin <109875785+Exedze@users.noreply.github.com>
2026-04-24 21:59:13 +0200

    knitting minigame features

---
+++ Assets/Scripts/Knit.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Knit : SneakyMinigame
+{
+    [SerializeField]
+    private int times;
+
+    [SerializeField] private List<GameObject> arrows = new List<GameObject>();
+
+    private int[] directions;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        directions = new int[times];
+        directions[0] = Random.Range(0, 3);
+        for (int i = 1; i < directions.Length; i++)
+        {
+            directions[i] = Random.Range(0, 3);
+            if (directions[i - 1] == directions[i])
+            {
+                directions[i]++;
+                directions[i] %= 4;
+            }   
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (progress >= times)
+        {
+            Destroy(gameObject);
+            DoingSecretStuff = false;
+            return;
+        }
+        checkProgress();
+        
+        //makes arrows look the right way
+        for (int i = 0; i < arrows.Count; i++)
+        {
+            //checks the equivalent index in the direction list
+            int directionEquiv = progress+i;
+            //make sure the direction exists
+            if (directionEquiv >= directions.Length)
+            {
+                arrows[i].SetActive(false);
+                continue;
+            }
+            arrows[i].SetActive(true);
+            //set rotation here dummy
+            arrows[i].transform.rotation=Quaternion.Euler(0,0,90*directions[directionEquiv]);
+        }
+    }
+
+    private void checkProgress()
+    {
+         
+        if (directions[progress] == 0 && (Keyboard.current.downArrowKey.wasPressedThisFrame || Keyboard.current.sKey.wasPressedThisFrame))
+        {
+            progress++;
+            return;
+        }
+        if (directions[progress] == 1 && (Keyboard.current.rightArrowKey.wasPressedThisFrame || Keyboard.current.dKey.wasPressedThisFrame))
+        {
+            progress++;
+            return;
+        }
+        if (directions[progress] == 2 && (Keyboard.current.upArrowKey.wasPressedThisFrame || Keyboard.current.wKey.wasPressedThisFrame))
+        {
+            progress++;
+            return;
+        }
+        if (directions[progress] == 3 && (Keyboard.current.leftArrowKey.wasPressedThisFrame || Keyboard.current.aKey.wasPressedThisFrame))
+        {
+            progress++;
+            return;
+        }
+    }
+}
--- Assets/Scripts/SneakyMinigame.cs
+++ Assets/Scripts/SneakyMinigame.cs
@@ -1,6 +1,18 @@
+using System;
 using UnityEngine;
 
 public class SneakyMinigame : MonoBehaviour
 {
     public static bool DoingSecretStuff = false;
+    protected int progress;
+
+    private void OnEnable()
+    {
+        DoingSecretStuff = true;
+    }
+
+    private void OnDisable()
+    {
+        DoingSecretStuff = false;
+    }
 }
